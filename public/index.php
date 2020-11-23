a73685526d34f52bbfd7b5e556a79399be0cec83
Mebale Noel <40523415+maestrodimateo@users.noreply.github.com>
2020-11-23 07:11:05 +0100

    add phpunit  and add files to gitignore

--- public/index.php
+++ public/index.php
@@ -4,12 +4,12 @@ use App\Http\RequestHandler\Router;
 use App\Http\Controllers\HomeController;
 use App\ExceptionsHandler\NotFoundException;
 
-include_once '../bootstrap.php';
+include_once  __DIR__ . '/../bootstrap.php';
 
 
-Router::get('accueil/vendre/:id/:name', []);
 Router::get('accueil/vendre/:id/', [HomeController::class, 'post']);
 Router::get('/accueil/vente', [HomeController::class, 'index']);
+Router::get('/accueil/vendredi', 'contact');
 Router::post('/accueil/vendre/:id/:name', 'view');
 Router::post('/post/update/:id', 'view');
 
