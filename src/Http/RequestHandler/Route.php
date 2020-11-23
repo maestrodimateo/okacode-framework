07489b86d990ee336be5f77ee15143fc3f26ab12
Mebale Noel <40523415+maestrodimateo@users.noreply.github.com>
2020-11-23 06:39:36 +0100

    Ajout des methode pour l'execution des routes

--- src/Http/RequestHandler/Route.php
+++ src/Http/RequestHandler/Route.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Http\RequestHandler;
 
+use App\Http\RequestHandler\Request;
+
 /**
  * @author mebale noel <noelmeb12@gmail.com>
  * @category routing
@@ -12,14 +14,7 @@ class Route
      *
      * @var string
      */
-    private $path = '/';
-
-    /**
-     * Les parametres de la requete
-     *
-     * @var array
-     */
-    private $params = [];
+    private $path;
 
     /**
      * l'action à executer
@@ -28,34 +23,94 @@ class Route
      */
     private $action;
 
-    public function __construct(string $path, string $callable)
+    private $params;
+
+    public function __construct(string $path, $callable)
     {
         $this->setPath($path);
         $this->setAction($callable);
     }
 
     /**
-     * Recupere le chemin et les paramètres
+     * Recupere le chemin
      *
      * @param string $path
      * @return self
      */
     private function setPath(string $path)
     {
-        $values = explode('/:', $path);
-        
-        $this->path = $values[0];
-        unset($values[0]);
-
-        if (count($values) > 1) {
-            $this->params = $values;
-        }
+        $this->path = trim($path, '/');
         return $this;
     }
 
+    /**
+     * Récupere l'action à executer
+     *
+     * @param mixed $action
+     * @return self
+     */
     private function setAction($action)
     {
-
+        $this->action = $action;
+        return $this;
     }
 
+    /**
+     * Execute le getter d'un attribut privé
+     *
+     * @param string $name : Le nom de l'attribut
+     * 
+     * @return \Exception|void
+     */
+    public function __get($name)
+    {
+        $method = 'set' . ucfirst($name);
+        $exist  = method_exists($this, $method);
+
+        if ($exist) {
+            return $this->$name;
+        }
+
+        throw new Exception("La propriété [$name] n'existe pas dans [$this]", 1);
+        
+    }
+
+    /**
+     * Compare le chemin de la requete à un chemin déclaré
+     *
+     * @param string $url : le chemin de la requete
+     * 
+     * @return boolean
+     */
+    public function match(string $url)
+    {
+        $path_pattern = preg_replace("/(:[\w]+)/", "([a-zA-Z0-9]+)", $this->path);
+        $path_match = "#^". $path_pattern ."$#";
+        $matching = preg_match($path_match, $url, $matches);
+
+        unset($matches[0]);
+        $this->params = $matches;
+
+        return ($matching) ? true : false;
+    }
+
+    /**
+     * L'action de la route
+     *
+     * @return void
+     */
+    public function execute(Request $request)
+    {
+        if (is_string($this->action)) {
+            echo "page static";
+            return;
+        }
+
+        if (is_array($this->action)) {
+            $this->action[0] = new $this->action[0];
+
+            return call_user_func($this->action, $request, ...$this->params);
+        }
+
+    }
 }
