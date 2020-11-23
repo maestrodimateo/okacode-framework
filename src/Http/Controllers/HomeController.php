d887df50945c8f3e6e8a293a24bf35d10d651933
Mebale Noel <40523415+maestrodimateo@users.noreply.github.com>
2020-11-23 22:33:15 +0100

    Test de priise en charge des variableq

--- src/Http/Controllers/HomeController.php
+++ src/Http/Controllers/HomeController.php
@@ -2,16 +2,33 @@
 namespace App\Http\Controllers;
 
 use App\Http\RequestHandler\Request;
+use App\Http\RequestHandler\Response;
 
 class HomeController
 {
     public function index()
     {
-        echo "Je suis dans la home";
+        $name = "noel";
+        return Response::render('form', compact('name'));
     }
 
     public function post(Request $request, int $id)
     {
         echo "le chemin est {$request->getPath()}, l'identifiant est: $id";
+    }
+
+    /**
+     * soumet le formulaire
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function getData(Request $request)
+    {
+        echo "<pre>";
+        var_dump($request->getBody());
+        echo "</pre>";
+
+        return Response::redirect('/');
     }
 }
