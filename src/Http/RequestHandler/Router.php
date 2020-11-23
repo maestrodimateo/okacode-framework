132fb2225c940a12c2576805a54635d1f98081b8
Mebale Noel <40523415+maestrodimateo@users.noreply.github.com>
2020-11-23 06:41:05 +0100

    Ajout des methodes pour verifier les routes dupliquées et l'execution

--- src/Http/RequestHandler/Router.php
+++ src/Http/RequestHandler/Router.php
@@ -1,6 +1,6 @@
 <?php
 namespace App\Http\RequestHandler;
-
+use App\ExceptionsHandler\NotFoundException;
 /**
  * Rédirige la requete vers la bonne route
  * @author mebale noel <noelmeb12@gmail.com>
@@ -18,7 +18,7 @@ namespace App\Http\RequestHandler;
 
 
     /**
-     * Enregitre une route de type get
+     * Enregistre une route de type get
      *
      * @param string $path : la requete de l'utilisateur
      * @param mixed $callable : L'action à executer
@@ -30,13 +30,13 @@ namespace App\Http\RequestHandler;
     }
 
     /**
-     * Enregitre une route de type post
+     * Enregistre une route de type post
      *
      * @param string $path : la requete de l'utilisateur
      * @param string $callable : L'action à executer
      * @return void
      */
-    public static function post(string $path, string $callable)
+    public static function post(string $path, $callable)
     {
         self::record_route($path, $callable, "POST");
     }
@@ -51,6 +51,46 @@ namespace App\Http\RequestHandler;
     private static function record_route(string $path, $callable, string $method = "GET")
     {
         $route = new Route($path, $callable);
+        self::cheDuplicatedRoutes($route, $method, $path);
         self::$routes[$method][] = $route;
+    }
+
+    /**
+     * Verifie s'il y a des routes dupliquées
+     *
+     * @param Route $currentRoute
+     * @param string $method
+     * @param string $longPath
+     * @return Exception|null
+     */
+    public static function cheDuplicatedRoutes(Route $currentRoute, string $method, string $longPath)
+    {
+        foreach (self::$routes[$method] as $route) {
+            if ($route->path === $currentRoute->path) {
+                throw new \Exception("La route [$longPath] de type [$method] existe déjà", 1);
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Execute le chargement de la route demandée
+     *
+     * @param Request $request
+     * 
+     * @return void
+     */
+    public static function run(Request $request)
+    {
+        foreach (self::$routes[$request->getMethod()] as $route) {
+           
+            if ($route->match($request->getPath())) {
+                return $route->execute($request);
+            }
+
+        }
+
+        throw new NotFoundException("Page introuvable");
     }
 }
