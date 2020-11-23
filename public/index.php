d5da3ffabc5476dbcc83543ba2da8ae00ba1ff8e
Mebale Noel <40523415+maestrodimateo@users.noreply.github.com>
2020-11-23 06:36:27 +0100

    Gestion des routes et de l'exception NotFoundException

--- public/index.php
+++ public/index.php
@@ -1,13 +1,18 @@
 <?php
 
 use App\Http\RequestHandler\Router;
-
-
+use App\Http\Controllers\HomeController;
+use App\ExceptionsHandler\NotFoundException;
 
 include_once '../bootstrap.php';
 
 
-Router::get('/accueil/vendre/:id/:name', 'view');
+Router::get('accueil/vendre/:id/:name', []);
+Router::get('accueil/vendre/:id/', [HomeController::class, 'post']);
+Router::get('/accueil/vente', [HomeController::class, 'index']);
+Router::post('/accueil/vendre/:id/:name', 'view');
+Router::post('/post/update/:id', 'view');
 
-phpinfo();
-die;
+try { Router::run($request); } 
+catch (NotFoundException $th) { $th->error404(); }
+
