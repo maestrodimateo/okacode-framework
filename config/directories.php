29b76b90625b8ecfb16be32d1c72a6f53d9aff02
Mebale Noel <40523415+maestrodimateo@users.noreply.github.com>
2020-12-09 12:19:19 +0100

    Separation between index and routes/web.php file

--- config/directories.php
+++ config/directories.php
@@ -1,6 +1,13 @@
 <?php
 
+/**
+ * File for directories configuration
+ * 
+ * @author mebale noel <noelmeb@gmail.com>
+ */
+
 define('BASE', substr($_SERVER['DOCUMENT_ROOT'], 0, -7));
 define('ASSETS', BASE . 'public/assets/');
 define('VIEWS', BASE . 'src/views/');
 define('COMPOSER', BASE . "vendor/autoload.php");
+define('ROUTES', BASE . "routes/");
--- public/index.php
+++ public/index.php
@@ -1,18 +1,11 @@
 <?php
 
 use App\Http\RequestHandler\Router;
-use App\Http\Controllers\HomeController;
 use App\ExceptionsHandler\NotFoundException;
 
 require_once  __DIR__ . '/../bootstrap.php';
 
-
-Router::get('/', [HomeController::class, 'index']);
-Router::get('/contact', 'frontend/complete');
-
-Router::get('/accueil/vendredi', 'contact');
-
-Router::post('/inscription', [HomeController::class, 'getData']);
+require_once ROUTES . "web.php";
 
 try {
     Router::run($request);
